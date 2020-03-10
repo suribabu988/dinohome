cfc67c52dcde2bb906c21bbb19b2ba4da8c69dcb
suribabu <suri.babu988@gmail.com>
2020-03-10 16:17:00 +0530

    ui changed

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
+	AOS.init({
+		easing: 'ease-in-out-sine'
+	});
 
 // Navbar
 	$(window).scroll(function() {
 		var height = $(window).scrollTop();
 	  if(height > 100) {
 			$('.menuButton').css('opacity','1');
+		  $('.menuButton').addClass('MenuslideOpen').removeClass('MenuslideClose');
 		} else {
 			$('.menuButton').css('opacity','0');
+		  $('.menuButton').addClass('MenuslideClose').removeClass('MenuslideOpen');
 		}
 		if (jQuery(window).width() < 1200 && jQuery(window).height() > 0) {
 			jQuery('.menuButton').css('opacity','1');
@@ -110,86 +115,17 @@ $('.menuFullScreen .right nav a').mouseleave(function(){
         }else{
             var elems = $('.item[data-type="'+this.value+'"]');
             $('.item').not(elems).hide();
-            elems.show();
+			elems.show();
+			$('.item-filter-sub-select').on('change', function () {
+				if (this.value == 'all') {
+					$('.subitem').show();
+				} else {
+					var elems = $('.subitem[data-subtype="' + this.value + '"]');
+					$('.subitem').not(elems).hide();
+					elems.show();
+				}			
+			});			
+
         }
     });
-	
-
-// 3D Mouse Effects
-	(function() {
-  var container = document.getElementById("container"),
-    inner = document.getElementById("inner");
-
-  var mouse = {
-    _x: 0,
-    _y: 0,
-    x: 0,
-    y: 0,
-    updatePosition: function(event) {
-      var e = event || window.event;
-      this.x = e.clientX - this._x;
-      this.y = (e.clientY - this._y) * -1;
-    },
-    setOrigin: function(e) {
-      this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
-      this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
-    },
-    show: function() {
-      return "(" + this.x + ", " + this.y + ")";
-    }
-  };
-
-  mouse.setOrigin(container);
-
-  //-----------------------------------------
-
-  var counter = 0;
-  var updateRate = 10;
-  var isTimeToUpdate = function() {
-    return counter++ % updateRate === 0;
-  };
-
-  //-----------------------------------------
-
-  var onMouseEnterHandler = function(event) {
-    update(event);
-  };
-
-  var onMouseLeaveHandler = function() {
-    inner.style = "";
-  };
-
-  var onMouseMoveHandler = function(event) {
-    if (isTimeToUpdate()) {
-      update(event);
-    }
-  };
-
-  //-----------------------------------------
-
-  var update = function(event) {
-    mouse.updatePosition(event);
-    updateTransformStyle(
-      (mouse.y / inner.offsetHeight / 2).toFixed(2),
-      (mouse.x / inner.offsetWidth / 2).toFixed(2)
-    );
-  };
-
-  var updateTransformStyle = function(x, y) {
-    var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
-    inner.style.transform = style;
-    inner.style.webkitTransform = style;
-    inner.style.mozTransform = style;
-    inner.style.msTransform = style;
-    inner.style.oTransform = style;
-  };
-
-  //-----------------------------------------
-
-  container.onmouseenter = onMouseEnterHandler;
-  container.onmouseleave = onMouseLeaveHandler;
-  container.onmousemove = onMouseMoveHandler;
-})();
-
-
-});
+	});
